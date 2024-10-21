20f923ea567d95bbeb0827e989f68db33ad25547
NetAcad DEVASC <changeme@example.com>
2024-10-21 15:07:06 +0000

    first commit

---
+++ graphhopper_parse-json_1.py
@@ -0,0 +1,133 @@
+import requests
+import urllib.parse
+
+route_url = "https://graphhopper.com/api/1/route?"
+key = "6b5dff90-1ceb-4903-9b24-ccbc1c200e06"
+
+import urllib.parse
+import requests
+main_api = "https://www.mapquestapi.com/directions/v2/route?"
+key = "g0AFs8HAGjGfE4t8nGqqYRQsAXDAVD6W"
+
+while True:
+    print ("")
+    print ("De: Grupo Concepción")
+    print ("")
+    print ("Para salir presione S")
+    print ("")
+    orig = input("Ingrese Ciudad de Origen: ")
+    if orig == "S" or orig == "s":
+        break
+
+    dest = input("Ingrese Ciudad de Destino: ")
+    if orig == "S" or orig == "s":
+        break
+
+    url = main_api + urllib.parse.urlencode({"key": key, "from":orig, "to":dest})
+    print("Datos de viaje desde", orig,"a",dest)
+
+    json_data = requests.get(url).json()
+    json_status = json_data["info"]["statuscode"]
+
+    if json_status == 0:
+        print("API Status: " + str(json_status) + " = A successful route call.\n")
+        print("=============================================")
+        print("Dirección desde " + (orig) + " a " + (dest))
+        print("Duración Viaje: " + (json_data["route"]["formattedTime"]))
+        print("Kilometros: " + str("{:.1f}".format((json_data["route"]["distance"])*1.61)))
+        print("=============================================")
+        for each in json_data["route"]["legs"][0]["maneuvers"]:
+            print((each["narrative"]) + " (" + str("{:.1f}".format((each["distance"])*1.61)) + " km)")
+        print("=============================================\n")
+
+
+def geocoding (location, key):
+    while location == "":
+        location = input("Enter the location again: ")
+    geocode_url = "https://graphhopper.com/api/1/geocode?" 
+    url = geocode_url + urllib.parse.urlencode({"q":location, "limit": "1", "key":key})
+
+    replydata = requests.get(url)
+    json_data = replydata.json()
+    json_status = replydata.status_code
+    
+    if json_status == 200:
+        json_data = requests.get(url).json()
+        lat=(json_data["hits"][0]["point"]["lat"])
+        lng=(json_data["hits"][0]["point"]["lng"])
+        name = json_data["hits"][0]["name"]
+        value = json_data["hits"][0]["osm_value"]
+        
+        if "country" in json_data["hits"][0]:
+            country = json_data["hits"][0]["country"]
+        else:
+            country=""
+        
+        if "state" in json_data["hits"][0]:
+            state = json_data["hits"][0]["state"]
+        else:
+            state=""
+        
+        if len(state) !=0 and len(country) !=0:
+            new_loc = name + ", " + state + ", " + country
+        elif len(state) !=0:
+            new_loc = name + ", " + country
+        else:
+            new_loc = name
+        
+        print("Geocoding API URL for " + new_loc + " (Location Type: " + value + ")\n" + url)
+    else:
+        lat="null"
+        lng="null"
+        new_loc=location
+    return json_status,lat,lng,new_loc
+
+while True:
+    print("\n+++++++++++++++++++++++++++++++++++++++++++++")
+    print("Vehicle profiles available on Graphhopper:")
+    print("+++++++++++++++++++++++++++++++++++++++++++++")
+    print("car, bike, foot")
+    print("+++++++++++++++++++++++++++++++++++++++++++++")
+    profile=["car", "bike", "foot"]
+    vehicle = input("Enter a vehicle profile from the list above: ")
+    if vehicle == "quit" or vehicle == "q":
+        break
+    elif vehicle in profile:
+        vehicle = vehicle
+    else: 
+        vehicle = "car"
+        print("No valid vehicle profile was entered. Using the car profile.")
+    loc1 = input("Starting Location: ")
+    if loc1 == "quit" or loc1 == "q":
+        break
+    orig = geocoding(loc1, key)
+    print(orig)
+    loc2 = input("Destination: ")
+    if loc2 == "quit" or loc2 == "q":
+        break
+    dest = geocoding(loc2, key)
+    print("=================================================")
+    if orig[0] == 200 and dest[0] == 200:
+        op="&point="+str(orig[1])+"%2C"+str(orig[2])
+        dp="&point="+str(dest[1])+"%2C"+str(dest[2])
+        paths_url = route_url + urllib.parse.urlencode({"key":key, "vehicle":vehicle}) + op + dp
+        paths_status = requests.get(paths_url).status_code
+        paths_data = requests.get(paths_url).json()
+        print("Routing API Status: " + str(paths_status) + "\nRouting API URL:\n" + paths_url)
+        print("=================================================")
+        print("Directions from " + orig[3] + " to " + dest[3] + " by " + vehicle)
+        print("=================================================")
+        if paths_status == 200:
+            miles = (paths_data["paths"][0]["distance"])/1000/1.61
+            km = (paths_data["paths"][0]["distance"])/1000
+            sec = int(paths_data["paths"][0]["time"]/1000%60)
+            min = int(paths_data["paths"][0]["time"]/1000/60%60)
+            hr = int(paths_data["paths"][0]["time"]/1000/60/60)
+            print("Distance Traveled: {0:.1f} miles / {1:.1f} km".format(miles, km))
+            print("Trip Duration: {0:02d}:{1:02d}:{2:02d}".format(hr, min, sec))
+            print("=================================================")
+            for each in range(len(paths_data["paths"][0]["instructions"])):
+                path = paths_data["paths"][0]["instructions"][each]["text"]
+                distance = paths_data["paths"][0]["instructions"][each]["distance"]
+                print("{0} ( {1:.1f} km / {2:.1f} miles )".format(path, distance/1000, distance/1000/1.61))
+            print("=============================================")
